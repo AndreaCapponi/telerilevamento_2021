2106544f0d4961797e9cb3c08bd354522715b821
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-03-17 12:21:10 +0100

    Update R_remote_code_sensing_first.r

--- R_remote_code_sensing_first.r
+++ R_remote_code_sensing_first.r
@@ -7,18 +7,18 @@
 #In R function setwd(''C:/lab/'') gives an error: unexpected symbol in (''C:/lab/'')
 #The correct and unexpected syntaxis' function is: setwd('C:/lab/')
 #After the association between R and lab folder, how I import data from lab folder to R?
-#I exploit  a function (brick) to import data from lab folder - external - to R - internal -
+#I exploit  a function (brick) to import data from lab folder - external - to R - internal* -
 #Syntaxis' function for Windows is: renowned object name <- brick('original object name')
-#Previous function is based on install.packages('raster'): I indicate to R trough library function to upload them as library(raster) where syntaxis doesn't need ' ' 
+#Previous function is based on install.packages('raster'): I indicate to R trough library function to upload them as library(raster) where syntaxis doesn't need ' '* 
 #Loading required packages: sp is successful
 #p224r63_2011_masked.grd is imported in R with brick function according to Windows' syntaxis in which: renowned object name = p224r63_2011 and original object name = p224r63_2011_masked.grd
 #Final syntaxis' function in Windows is: p224r63_2011 <- brick('p224r63_2011_masked.grd')
 #How to visualize informations of p224r63_2011 in R? 
 #In R, visualizing informations of an object, name of it followed by enter as physical command by keyboard
 #If I want to obtain visually frequence bands in p224r63_2011?
-#I exploit  a function (plot) to obtain visually frequence bands
+#I exploit  a function (plot) to obtain visually spectral bands
 #Syntaxis' function for Windows is: plot(object)
-#In R function plot(p224r63_2011) lead me to a schedule with frequence bands explained graphically 
+#In R function plot(p224r63_2011) lead me to a schedule with spectral bands explained graphically 
 #Sequence of informatic commands for R_remote_code_sensing_first.r
 
 install.packages('raster')
