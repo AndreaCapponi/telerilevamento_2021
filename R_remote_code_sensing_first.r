17b7261817060277c4919f6c1fc193b0699c2f7e
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-03-17 11:00:28 +0100

    Create R_remote_code_sensing_first.r

---
+++ R_remote_code_sensing_first.r
@@ -0,0 +1,32 @@
+#Title for R's developing code
+#My first R code for remote sensing
+#Which folder must R use in the computer?
+#In the computer R must use the folder named simply lab
+#I exploit a function (setwd) for the association between R and lab folder
+#Syntaxis' function for Windows is: setwd(''C:/lab/'')
+#In R function setwd(''C:/lab/'') gives an error: unexpected symbol in (''C:/lab/'')
+#The correct and unexpected syntaxis' function is: setwd('C:/lab/')
+#After the association between R and lab folder, how I import data from lab folder to R?
+#I exploit  a function (brick) to import data from lab folder - external - to R - internal -
+#Syntaxis' function for Windows is: example 1 <- brick('example 2')
+#Previous function is based on install.packages('raster'): I indicate to R trough library function to upload them as library(raster) where syntaxis doesn't need ' ' 
+#Loading required packages: sp is successful
+#p224r63_2011_masked.grd is imported in R with brick function according to Windows' syntaxis in which: example 1 = p224r63_2011 and example 2 = p224r63_2011_masked.grd
+#Final syntaxis' function in Windows is: p224r63_2011 <- brick('p224r63_2011_masked.grd')
+#How to visualize informations of p224r63_2011 in R? 
+#In R, visualizing informations of an object, name of it followed by enter as physical command
+#In p224r63_2011 I want to obtain visually frequence bands
+#I exploit  a function (plot) to obtain visually frequence bands
+#Syntaxis' function for Windows is: plot(object)
+#In R function plot(p224r63_2011) lead me to a schedule with frequence bands explained graphically 
+#Sequence of informatic commands for R_remote_code_sensing_first.r
+
+install.packages('raster')
+
+library(raster)
+
+setwd('C:/lab/')  
+
+p224r63_2011 <- brick('p224r63_2011_masked.grd')
+
+plot(p224r63_2011)
