d7f4e0f6d9faa72aa3344f1f8031b87c249655fe
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-03-31 13:00:05 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,10 @@
+#Code time series 
+#Greenland increases of temeprature
+#Data and code from Emanuela Cosma
+#library
+#setwd# install.packages("raster")
+library(raster)
+ 
+setwd("~/lab/greenland") # Linux
+# setwd("C:/lab/greenland") # Windows
+# setwd("/Users/name/Desktop/lab/greenland") # Mac
