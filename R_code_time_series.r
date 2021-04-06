05487486addd8b17b292a3cb347ba656c32dc097
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-04-06 12:24:26 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -8,3 +8,13 @@ library(raster)
 setwd("~/lab/greenland") # Linux
 # setwd("C:/lab/greenland") # Windows
 # setwd("/Users/name/Desktop/lab/greenland") # Mac
+> install.packages("RStoolbox")
+provo con l'URL 'https://cran.mirror.garr.it/CRAN/bin/windows/contrib/4.0/RStoolbox_0.2.6.zip'
+Content type 'application/zip' length 2170101 bytes (2.1 MB)
+downloaded 2.1 MB
+
+package ‘RStoolbox’ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+        C:\Users\PC\AppData\Local\Temp\RtmpEx7d3z\downloaded_packages
+> library(RStoolbox)
