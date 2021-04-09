fb1a877bbb1da479ab8b19c83c0fe878fe84d4b5
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-04-09 10:55:47 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -78,6 +78,35 @@ plotRGB(TGr, 2, 3, 4, stretch="Lin")
 con dei valori immagini dasatellite per i valori della temperatura!
 plotRGB(TGr, 4, 3, 2, stretch="Lin")
 
+#Al di sotto di questo commento studiare e organizzare la lezione dell' 08/04/2021
+
+#Visualitazione methods for raster data
+#install.packages('rasterVis')
+#library(rasterVis)
+#setwd("C:/lab/greenland")
+#rlist <- list.files(pattern="lst")
+#rlist
+#import <- lapply(rlist,raster)
+#import
+#TGr <- stack(import)
+#TGr
+#levelplot(TGr)
+#levelplot(TGr$lst_2000)
+#cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+#levelplot(TGr, col.regions=cl)
+#levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#meltlist <- list.files(pattern="melt")
+#melt_import <- lapply(meltlist,raster)
+#melt <- stack(melt_import)
+#melt
+#levelplot(melt)
+#clb <- colorRampPalette(c("blue","white","red"))(100)
+#plot(melt_amount, col=clb)
+#melt_amount
+#levelplot(melt_amount, col.regions=clb)
+
 #Sequence of informatic commands for R_code_time_series.r:
 
 install.packages('raster')
@@ -90,7 +119,7 @@ install.packages("rgdal")
 
 library(raster)
 
-setwd('C:/greenland/')  
+setwd("C:/lab/greenland/")
 
 lst_2000 <- raster('lst_2000.tif')
 
