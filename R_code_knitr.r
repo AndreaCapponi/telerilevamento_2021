d68a8e7b3871b5a4e62518b6972806688c5cc8f1
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-04-16 09:27:21 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,8 @@
+#Al di sotto di questo commento studiare e organizzare la lezione del 14/04/2021
+#install.packages("RStoolbox")
+#library(RStoolbox)
+#install.packages('knitr', dependencies = TRUE)
+#library(knitr)
+#stitch function is required!
+#stitch: Automatically create a report based on an R script and a template
+#This is a convenience function for small-scale automatic reporting based on an R script and a template. The default template is an Rnw file (LaTeX); stitch_rhtml() and stitch_rmd() are wrappers on top of stitch() using the R HTML and R Markdown templates respectively.
