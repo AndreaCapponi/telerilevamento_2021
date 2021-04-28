fc631b92a4a1805d630174841467411e297430ed
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-04-28 12:57:34 +0200

    Update and rename R_code_ecosystem_functions.r to R_code_vegetation_indices.r

--- R_code_ecosystem_functions.r
+++
@@ -1,1 +0,0 @@
-#Al di sotto di questo commento studiare e organizzare la lezione del 28/04/2021
---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,13 @@
+#Al di sotto di questo commento studiare e organizzare la lezione del 28/04/2021
+#library(raster)
+#setwd("C:/lab/") 
+#defor1 <- brick("defor1.jpg")
+#defor2 <- brick("defor2.jpg")
+#banda 1 = NIR, banda 2 = red e banda 3 = green
+#par(mfrow=c(2,1))
+#plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+#plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+#
+#
+#
+#
