0f36d8bf9a49184f8d6cccbcdfed8ae31c29d9ae
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-05-21 10:51:29 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -62,12 +62,87 @@
 #Quanta variabilità iniziale spiegano le singole componenti?
 #summary(sentinelpca$map)
 #Il PC1 spiega il 67,368% dell'informazione contenuta nell'immagine originale!
+
+#Al di sotto commentare e organizzare la lezione del 21/05/2021
+#Le operazioni preliminari per proseguire nel codice della lezione precedente sono:
+#library(raster)
+#library(RStoolbox)
+#setwd("C:/lab/")
+#sentinel <- brick("sentinel.png")
+#sentinelpca <- rasterPCA(sentinel)
+#sentinelpca
+#summary(sentinelpca$map)
+#Su quale banda vogliamo agire?
+#Com'è fatta la sua mappa?
+#La prima componente si chiama PC1. Come usare eslusivamente la prima componente?
+#PC1 <-sentinelpca$map$PC1
+#Qualsiasi funzione in R è da rinominare in oggetti che sono maggiormente gestibili rispetto a stringhe di codice
+PC1sd5 <- focal(PC1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100)
+plot(PC1sd5, col=clsd)
+PC1sd13 <- focal(PC1, w=matrix(1/169, nrow=13, ncol=13), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100)
+plot(PC1sd13, col=clsd)
+#Forte variabilità geo-morfologica imputabile alla presenza di creste, crepacci, ...
+#La vegetazione rappresentata dal colore blu è omogenea e va a rappresentare una o più praterie mentre l'aumento di variabilità è osservabile nella transazione tra praterie e rocce
+#La funzione source richiama un pezzo di codice che si è creato
+#source: Read R Code from a File, a Connection or Expressions
+#source causes R to accept its input from the named file or URL or connection or expressions directly. Input is read and parsed from that file until the end of the file is reached, then the parsed expressions are evaluated sequentially in the chosen environment.
+#Nella cartella lab io ho source_test_lezione.rFile
+#source("source_test_lezione.r")
+#Error in eval(ei, envir) : oggetto "sentpca" non trovato
+#La mia pca si chiama sentinelpca, è da modificare il file che il professore ha caricato su Virtuale
+#library(ggplot2)
+#library(gridExtra)
+#install.packages("viridis")
+#library(viridis) for ggplot2 coloring
+#source("source_ggplot.r")
+#La correzione dei nomi fa agire la funzione indipendentemente dal creatore di questa stringa di codice!
+#Inizialmente si deve creare una finestra all'interno della funzione ggplot come ggplot() (creazione di un'area di lavoro completamente vuota)
+#L'aggiunta di elementi in ggplot2 avviene per blocchi, quindi dopo ggplot() segue la somma come operazione +
+#Poi è da definirsi la geometria dipendentemente dal grafico che voglio creare tramite questa funzione. Essendo pixels io sfrutto la funzione è geom_raster()
+#La sintassi della funzione finora è ggplot() +
+geom_raster(PC1sd5, ...)
+#L'estetiche sono definite attraverso il 
+#La sintassi aggiornata è:ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))
+#Questo è il metodo migliore per indentificare a livello geografico delle discontinuita, in ambito geologico qualsiasi variabilità mentre in ecologia l'ecotonia
+#Qual è il punto con la maggiore variabilità? Maggiore variabilità geo-morfologica in alto a sinistra
+#https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+# The package contains eight color scales: “viridis”, the primary choice, and five alternatives with similar properties - “magma”, “plasma”, “inferno”, “civids”, “mako”, and “rocket” -, and a rainbow color map - “turbo”
+#Come si chiama la funzione per creare le legende per ciascun grafico in ggplot?
+#ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))
+#Come aggiungere la legenda di default per il pacchetto viridis?
+ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis()
+#Come aggiungere un titolo? L'argomento non è più main ma ggtitle
+#ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis()+ggtitle("Standard deviation of PC1 by viridis colour scale")
+#ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis(option="magma")+ggtitle("Standard deviation of PC1 by magma colour scale")
+#scale_fill_viridis, search on Google because is an option for another legend
+#ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis(option="inferno")+ggtitle("Standard deviation of PC1 by inferno colour scale")
+#ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis(option="turbo")+ggtitle("Standard deviation of PC1 by turbo colour scale")
+#Tre mappe disponibili: viridis, magma e turbo a cui associo loro dei nomi per creare successivamente un grid arrange
+p1 <- ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis()+ggtitle("Standard deviation of PC1 by viridis colour scale")
+p2 <- ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis(option="magma")+ggtitle("Standard deviation of PC1 by magma colour scale")
+p3 <- ggplot() +
+geom_raster(PC1sd5, mapping = aes( x = x, y = y, fill = layer))+
+scale_fill_viridis(option="turbo")+ggtitle("Standard deviation of PC1 by turbo colour scale")
+#grid.arrange(p1, p2, p3, nrow=1)
 #
 #
-#
-#
-#
-#
-#
-#
-#
+
+
