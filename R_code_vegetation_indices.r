73bf9eb34de220e1590c157e8b2a705e645ea03a
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-05-25 16:18:29 +0200

    Update R_code_vegetation_indices.r

--- R_code_vegetation_indices.r
+++ R_code_vegetation_indices.r
@@ -22,26 +22,45 @@
 #Syntaxis' function for Windows is:par(multiframe, a graphical visualization of multiple object's level of ''interest'' organized by r̲o̲w̲s or c̲o̲l̲umns at the user's discretion = array⁴(number of rows or columns required, number of columns or rows required))
 #Final syntaxis' function in Windows is: par(mfrow=c(2,1)) which has a functional significance if and only followed by plotRGB(defor1, r=1, g=2, b=3, stretch="lin") and plotRGB(defor2, r=1, g=2, b=3, stretch="lin")    
 
-#Qual è il nome delle bande per le bande multispettralli del vicino all'infrarosso, rosso e verde?
-
-#Al di sotto di questo commento studiare e organizzare la lezione del 30/04/2021
+↓
 
 #The study by remote sensing of deforestation in the dry tropical forests of Mato Grosso (NT0140) is based on vegetation indices (VIs)
 #What is a vegetation index (VI)?
+
 #A vegetation index (VI) is a measure of vegetation properties calculated by the amount of solar radiation reflected in the optical spectrum
+
 #The spectral signature of healthy vegetation depends on the higher reflectance values of 0,7 to 1,1 µm in the near infrared (NIR) region than in the visible region due to the cellular structure of the leaves - in particular the spongy mesophyll - where almost all of the light is absorbed by photosynthetic pigments. Chlorophyll intensely absorbs energy in the blue and red wavelengths from 0,4 a 0,7 µm, reflecting in the green wavelength instead. Finally, the reflectance in the short wave infrared (SWIR) wavelengths is related to the water content of the vegetation with absorption bands around 1.45, 1.95 and 2.50 µm. Outside of these absorption bands, leaf reflectance generally increases as the water content in the leaf decreases!
 #DVI (D͟ifference V͟egetation I͟ndex) is the simplest vegetation index calculated as the difference between the near-infrared and red values: DVI = NIR - R
-#The ASTER sensor of the Earth satellite (EOS AM-1) has channels for wavelength values in the NIR and R bands through which the reflectance values can be obtained to calculate the DVI index
-#The name of the NIR and R bands in defor1 can be found in its information obtained by pressing the ↵ Enter key on the physical keyboard
+#The ASTER sensor of the Terra satellite (EOS AM-1) has 14 channels for wavelength values in the visible, NIR, SWIR and LWIR. Through the bands B1, B2, B3N and B3B belonging to the visible and NIR - because their wavelengths are between 0.520 µm and 0.860 µm - it is possible to obtain the reflectance values necessary for the calculation of the DVI index in dry tropical forests del Mato Grosso (NT0140)
+
+#How to visualize informations of defor1 in R? 
+#In R, visualizing informations of an object, name of it followed by Enter as physical command by keyboard
+#Syntaxis' function for Windows is: object name ↵ Enter
+#Final syntaxis' function in Windows is: defor1 ↵ Enter
+
+#I initially associate the channels for the NIR and the R of the ASTER sensor on the Earth satellite (EOS AM-1) to defor1 and defor2 to get the reflectance values in those bands of the electromagnetic spectrum
 #I associate the NIR and R bands with the names defor1.1 and defor1.2 to the original image defor1 through the $ function being my two levels of "interest"
+#The bands of my interest will be for defor1, defor.1.1 and defor1.2 that are associated with them through the $ function
+#If the user requires adapted R's function plot(p224r63_2011, col=rmpcp) will display graphically reflectance's values for one of the previous spectral bands (B1:blue, B2:green, B3:red, B4:near-infrared, B5:mid-infrared, B6:far-infrared or B7:other mid-infrared)? 
+#I exploit a function ($) to extract, from the initial (plot)¹ of an object represented by a matrix of data, a set of them that will allow the user to visualize graphically the level of ''interest''
+#Initial syntaxis' function for Windows: plot(renowned object name$name of the object's level in which the user is interested for graphical visualization)
+#What will be the final syntaxis' function in Windows if my p224r63_2011's level of ''interest''² is the set of data which will be graphically visualized in their functional whole being as the spectral band B1:blue?
+#In progress syntaxis' function in Windows is: plot(p224r63_2011$B1_sre) 
+#With plot(p224r63_2011$B1_sre) the user graphically will visualize the spectral band B1_sre:blue stand-alone² in its reflectance's values which colorscale is ax expected by default in R itself
+#Insert contextualized dollar function
+#Name and band of the dataset to link to the dollar function
+#defor1 $ defor1.1, first band for NIR!
+#defor1 $ defor1.2, second tier for RED!
+#For each defor1 pixel I am considering the value in the NIR band and the value in the R band in difference. From this operation we will obtain a map formed by pixels in difference with respect to the previous values!
+#dvi1 <- defor1 $ defor1.1 - defor1 $ defor1.2
+#plot (dvi1)
+#State of health, yellow meh green excellent. Change color to better appreciate this vegetation index!
+#cl <- colorRampPalette (c ('dark blue', 'yellow', 'red', 'black')) (100)
+#plot (dvi1, col = cl)
+#plot (dvi1, col = cl, main = "DVI at time 1")
 #The calculation of the DVI index is according to the formula a difference between defor1 $ defor1.1 and defor1 $ 1.2 to represent for each pixel - which constitutes the original image defor1 - the reflectance value as the difference between those associated with the NIR and R bands!
-#The plot of dvi1 shows the state of health of the vegetation of the dry forests of Mato Grosso through reflectance values ​​measured by the ASTER sensor of the Earth satellite (EOS AM-1) where the more the green is intense, the more the trees of the aforementioned forest are in physiologically and ecologically optimal conditions
-#
-#
-#
-#
-#
-#
+#The plot of dvi1 shows the state of health of the vegetation of the dry forests of Mato Grosso through reflectance values measured by the ASTER sensor of the Earth satellite (EOS AM-1) where the more the green is intense, the more the trees of the aforementioned forest are in physiologically and ecologically optimal conditions
+
 
 
 
@@ -50,10 +69,7 @@
 #NDVI (N͟ormalized D͟ifference V͟egetation I͟ndex) is another vegetation index calculated as the ratio between the difference (-) and the sum (+) of the near-infrared and red values respectively to the numerator and denominators: NDVI = (NIR – R)/(NIR + R)
 – Ranges from -1 to 1
 
-#How to visualize informations of defor1 in R? 
-#In R, visualizing informations of an object, name of it followed by Enter as physical command by keyboard
-#Syntaxis' function for Windows is: object name ↵ Enter
-#Final syntaxis' function in Windows is: defor1 ↵ Enter
+
 #
 
 #DVI1, come si chiamano le bande per NIR E RED?
@@ -71,33 +87,6 @@
 #plot(dvi1, col=cl)
 #plot(dvi1, col=cl, main="DVI at time 1")
 
-#I initially associate the channels for the NIR and the R of the ASTER sensor on the Earth satellite (EOS AM-1) to defor1 and defor2 to get the reflectance values in those bands of the electromagnetic spectrum
-#Their name is viewable in the names category for general information
-#How to view information of defor1 in R?
-#In R, display the information of an object, its name followed by Enter as a physical keyboard command
-#The syntax function for Windows is: object name ↵ Enter
-#The final syntax function in Windows is: defor1 ↵ Enter
-#The bands of my interest will be for defor1, defor.1.1 and defor1.2 that are associated with them through the $ function
-#If the user requires adapted R's function plot(p224r63_2011, col=rmpcp) will display graphically reflectance's values for one of the previous spectral bands (B1:blue, B2:green, B3:red, B4:near-infrared, B5:mid-infrared, B6:far-infrared or B7:other mid-infrared)? 
-#I exploit a function dev.off() to end a graphical display depending on the R's (plot) function and if it is syntactically correct, the user will see this message appears: null device 1
-#Error in dev.off() : cannot shut down the device 1 (dispositivo null) message appears exclusively when the user has closed manually graphical display's window clicking on the x at the top right
-#I exploit a function ($) to extract, from the initial (plot)¹ of an object represented by a matrix of data, a set of them that will allow the user to visualize graphically the level of ''interest''
-#Initial syntaxis' function for Windows: plot(renowned object name$name of the object's level in which the user is interested for graphical visualization)
-#What will be the final syntaxis' function in Windows if my p224r63_2011's level of ''interest''² is the set of data which will be graphically visualized in their functional whole being as the spectral band B1:blue?
-#In progress syntaxis' function in Windows is: plot(p224r63_2011$B1_sre) 
-#With plot(p224r63_2011$B1_sre) the user graphically will visualize the spectral band B1_sre:blue stand-alone² in its reflectance's values which colorscale is ax expected by default in R itself
-#Insert contextualized dollar function
-#Name and band of the dataset to link to the dollar function
-#defor1 $ defor1.1, first band for NIR!
-#defor1 $ defor1.2, second tier for RED!
-#For each defor1 pixel I am considering the value in the NIR band and the value in the R band in difference. From this operation we will obtain a map formed by pixels in difference with respect to the previous values!
-#dvi1 <- defor1 $ defor1.1 - defor1 $ defor1.2
-#plot (dvi1)
-#State of health, yellow meh green excellent. Change color to better appreciate this vegetation index!
-#cl <- colorRampPalette (c ('dark blue', 'yellow', 'red', 'black')) (100)
-#plot (dvi1, col = cl)
-#plot (dvi1, col = cl, main = "DVI at time 1")
-
 #The range of wavelengths measured by a sensor is known as a band and are commonly described by the name (Red or Near-IR for example) and the wavelength of the energy being recorded
 
 
@@ -112,6 +101,8 @@
 #par(mfcol=c(1,2)) o par(mfrow=c(2,1))
 #plot(dvi1, col=cl, main="DVI at time 1")
 #plot(dvi2, col=cl, main="DVI at time 2")
+
+
 #Qual è stata la differenza nella stessa zona in due lassi temporali differenti?
 #difdvi <- dvi1 - dvi2
 #In dvi1 - dvi2 :
