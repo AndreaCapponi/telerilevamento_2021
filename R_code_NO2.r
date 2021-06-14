3cf89e6bc5880210f6fddff11700ef8832a7748a
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-06-14 10:33:10 +0200

    Create R_code_NO2.r

---
+++ R_code_NO2.r
@@ -0,0 +1,12 @@
+# 1. Set working directory EN
+#Which is the title for R's developing code?
+#My R code for a time series
+#Which folder must R use in the computer for this new code?
+#In the computer R must use the subfolder named greenland within the "mother" folder simply called lab
+#I exploit a function (setwd) for the association between R and greenland subfolder
+#Adapted syntaxis' function for Windows is: setwd("C:/name of the folder in which there is a subfolder of interest to the user/name of the subfolder which will be associated with R")
+#Final syntaxis' function for Windows is: setwd("C:/lab/greenland")
+library(raster)
+
+setwd("C:/lab/EN/")
+#
