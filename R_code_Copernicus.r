14d1871c7c6a7035132f73a7836dd1f28d35aedc
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-07-28 15:57:57 +0200

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,58 +1,31 @@
 #What is Copernicus? 
 #Copernicus is the European Union’s Earth Observation Programme, which monitors our planet and its environment for the ultimate benefit of the citizens of Europe. It delivers data, information and services based on satellite Earth Observation data and insitu (non-space) data. The Programmeis funded, coordinated and managedby the European Commission in cooperation with partners such as ESA and EUMETSAT
-#What is the official Copernicus website to connect to?
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-#What is the official Copernicus - European Union’s Earth Observation Programme website?
-#The official Copernicus - European Union’s Earth Observation Programme website is https://www.copernicus.eu/en
-#
+#What is the official Copernicus website?
+#The official Copernicus website is https://www.copernicus.eu/en
+#How to register a personal account on this website?
+#To register a personal account on the previous website anyone have to click the 𝗥𝗘𝗚𝗜𝗦𝗧𝗘𝗥 button and enter the following informations in the dedicated sections: 𝘜𝘴𝘦𝘳𝘯𝘢𝘮𝘦*, 𝘗𝘢𝘴𝘴𝘸𝘰𝘳𝘥*, 𝘊𝘰𝘯𝘧𝘪𝘳𝘮 𝘱𝘢𝘴𝘴𝘸𝘰𝘳𝘥*, 𝘍𝘪𝘳𝘴𝘵 𝘯𝘢𝘮𝘦*, 𝘓𝘢𝘴𝘵 𝘯𝘢𝘮𝘦*, 𝘌-𝘮𝘢𝘪𝘭 𝘢𝘥𝘥𝘳𝘦𝘴𝘴*, 𝘊𝘰𝘮𝘱𝘢𝘯𝘺*, 𝘋𝘰𝘮𝘢𝘪𝘯 𝘰𝘧 𝘢𝘤𝘵𝘪𝘷𝘪𝘵𝘺*, 𝘛𝘺𝘱𝘦 𝘰𝘧 𝘰𝘳𝘨𝘢𝘯𝘪𝘻𝘢𝘵𝘪𝘰𝘯* and 𝘊𝘰𝘶𝘯𝘵𝘳𝘺*
+#After completing the 𝘎𝘦𝘯𝘦𝘳𝘢𝘭 𝘪𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 section, the future user will prove that he/she is not a robot throught reCAPTCHA system in the 𝘝𝘢𝘭𝘪𝘥𝘢𝘵𝘪𝘰𝘯 𝘤𝘰𝘥𝘦 section below: clicking on the box a green check (G✓) should appear! 
+#To conclude the 𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘢𝘵𝘪𝘰𝘯 𝘧𝘰𝘳𝘮, in the 𝘛𝘦𝘳𝘮𝘴 𝘰𝘧 𝘶𝘴𝘦 section in the end terms of use and privacy policy must be accepted by ticking with the left mouse button another box but this time with a blue check (B✓)     
+#Clicking the 𝗥𝗘𝗚𝗜𝗦𝗧𝗘𝗥 button should appear the following message: By registering in this website you are deemed to have accepted the T&C for Sentinel data use
+#After this message appears, the user will receive an account activation link in the mailbox indicated in the appropriate section with the information previously entered
+#Attention: the activation of an account in the Copernicu programme is a prerequisite for downloading satellite images on it
 #How to download a satellite image from the official website of the Copernicus program?
-#To download a satellite image of the Copernicus program, I have to visit the website indicated in the string below ↓
-
-
-
-#How to register a personal account on the website?
-#Sign up
-#Firstname, lastname, username, password, confirm password, email, confirm email, select domain, select usage, select country
-
-#By registering in this website you are deemed to have accepted the T&C for Sentinel data use
-
-#Register
-
-#Vegetation state, Energy budget, Water Cycle and Cryosphere
-
-#Vegetation State
-	
-VEGETATION PROPERTIES VEGETATION INDICATORS DRY MATTER PRODUCTIVITY 	
-FIRE DISTURBANCE SOIL WATER INDEX 	
-SURFACE SOIL MOISTURE
-
-ROOT - TOP OF CANOPY REFLECTANCE 	
-ALBEDO
-
-LAND SURFACE TEMPERATURE
-
-LAKE SURFACE WATER TEMPERATURE 	
-LAKE WATER QUALITY WATER BODIES WATER LEVEL
-
-CRYOSPHERE
-
-#Collections, prodotti disponibili da scaricare in anteprima
-#i, informazioni sul tema
-
-#Help The "Catalogue search" tab allows you to define search criteria and start a search within the catalogue
-#Date  Start date End Date 
-# Roi coordinate geografiche della zona di interesse
-#Search in progress please wait
+#The satellite image that the user would like to download is to be identified within the thematic sections contained in the main page: 𝘝𝘦𝘨𝘦𝘵𝘢𝘵𝘪𝘰𝘯 𝘴𝘵𝘢𝘵𝘦, 𝘌𝘯𝘦𝘳𝘨𝘺 𝘣𝘶𝘥𝘨𝘦𝘵, 𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 and 𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦
+#Each thematic section is made up of collections which, in order, are:
+#𝘝𝘦𝘨𝘦𝘵𝘢𝘵𝘪𝘰𝘯 𝘴𝘵𝘢𝘵𝘦 -> VEGETATION PROPERTIES VEGETATION INDICATORS DRY MATTER PRODUCTIVITY, FIRE DISTURBANCE SOIL WATER INDEX and SURFACE SOIL MOISTURE
+#𝘌𝘯𝘦𝘳𝘨𝘺 𝘣𝘶𝘥𝘨𝘦𝘵 -> ROOT - TOP OF CANOPY REFLECTANCE, ALBEDO and LAND SURFACE TEMPERATURE
+#𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 -> LAKE SURFACE WATER TEMPERATURE, LAKE WATER QUALITY,WATER BODIES and WATER LEVEL
+#𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦 -> CRYOSPHERE
+#It is possible to have a preview of the collection in which I am interested by clicking on the Collections subsection;
+# The 𝓲 at the top right instead is a link to the European Union's Earth Observation Program website where the collection I selected from the main page is in-depth;
+# By clicking on the collection from which to obtain the satellite image, a map of the globe will appear on the screen with a toolbar on the right to extrapolate the image of my interest
+#The reference section is Catalog research with three subsections to facilitate the search for the user: Help, Collection and Basic
+#In the Help subsection there is the following warning: The "Catalog search" tab allows you to define search criteria and start a search within the catalog
+#In the Collection subsection there is the set of downloadable products in the reference collection
+# While in the Basic subsection the user has the possibility to choose the time period in which to carry out a hypothetical future analysis: Start date and End Date. However, with the Slot I can be in a position to investigate a range from year to year useful for multitemporal analyzes
+#The ROI instead is a tool that allows you to enter the coordinates of the area of ​​my interest or through which I get them after selecting the previous area as Draw region of interest
+#Search in progress wait
+# To download the satellite image of my interest - visible in preview by clicking with the left button on the thumbnail or even quicklook for a correspondence with the global map made available by Copernicu - the blue download arrow is responsible for uploading it to the indicated folder during his rescue
 
 #Visualizzaione di dati dal programma Copernicus Europe's eyes on Earth
 #Iscrizione al programma Copernicus Europe's eyes on Earth tramite sito web
