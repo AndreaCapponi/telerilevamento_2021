cf6923617182ad7584d1ee447f69d9d0d7140cd0
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-08-04 07:10:25 +0200

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -13,7 +13,9 @@
 #9. R code land cover
 #10.R code variability
 #----------------------------------------------
+
 #1. Remote Sensing - first code
+
 #Title for R's developing code
 #My first R code for remote sensing
 #Which folder must R use in the computer?
@@ -476,4 +478,88 @@ plot(melt_amount, col=bwrcp)
 
 levelplot(melt_amount, col.regions=bwrcp)
 
+#----------------------------------------------
+
+#4. R code knitr
+
+#install.packages() is a function which download and install packages from CRAN-like repositories or from local files
+
+#The first package to install is RStoolbox, a toolbox for remote sensing image processing and analysis such as calculating spectral indices, principal component transformation, unsupervised and supervised classification or fractional cover analyses:
+
+install.packages("RStoolbox")
+
+#The second package to install is knitr, a general-purpose tool for dynamic report generation in R using Literate Programming techniques:
+
+install.packages('knitr', dependencies = TRUE)
+
+#library() is a function which load and attach add-on packages, in this case the previous RStoolbox and knitr packages:
+
+library(RStoolbox)
+
+library(knitr)
+
+#I might as well consider require() function, giving a warning message and returning a logical value as FALSE if the requested package is not found and TRUE if the package is loaded. The library() function instead by default returns an error if the requested package does not exist!
+
+#In my computer R must use the folder named simply lab and I exploit a function (setwd) for the association between R and lab folder:
+
+setwd("C:/lab/")
+
+# stitch() is a convenience function that creates automatically a small-scale report based on a R script and a template:
+
+stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#The future script will be R_code_Greenland.r.txt and the default template is an Rnw file (LaTeX); stitch_rhtml() and stitch_rmd() are wrappers on top of stitch() using the R HTML and R Markdown templates respectively!
+
+#knitr is conceptually internal to R but in creating a report it is based on an external code (inverted commas " "). After importing it into R, this function saves the complete code as a text document inside the folder - indicated instead with the previous setwd () function - with a title corresponding to the name of the selected repository (R_code_Greenland)
+
+#Sequence of informatic commands for R_code_knitr.r
+
+install.packages("RStoolbox")
+
+install.packages('knitr', dependencies = TRUE)
+
+library(RStoolbox)
+
+library(knitr)
+
+setwd("C:/lab/")
+
+stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+
 #-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
