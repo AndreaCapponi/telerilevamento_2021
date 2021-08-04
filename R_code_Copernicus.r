8c9d05b225273b9239ba58ce0d2086cc755f5a2e
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-08-04 06:20:38 +0200

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -8,7 +8,7 @@
 #To conclude the 𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘢𝘵𝘪𝘰𝘯 𝘧𝘰𝘳𝘮, in the 𝘛𝘦𝘳𝘮𝘴 𝘰𝘧 𝘶𝘴𝘦 section in the end terms of use and privacy policy must be accepted by ticking with the left mouse button another box but this time with a blue check (B✓)     
 #Clicking the 𝗥𝗘𝗚𝗜𝗦𝗧𝗘𝗥 button should appear the following message: By registering in this website you are deemed to have accepted the T&C for Sentinel data use
 #After this message appears, the user will receive an account activation link in the mailbox indicated in the appropriate section with the information previously entered
-#Attention: the activation of an account in the Copernicu programme is a prerequisite for downloading satellite images on it
+#Attention: the activation of an account in the Copernicus programme is a prerequisite for downloading satellite images on it
 #How to download a satellite image from the official website of the Copernicus program?
 #The satellite image that the user would like to download is to be identified within the thematic sections contained in the main page: 𝘝𝘦𝘨𝘦𝘵𝘢𝘵𝘪𝘰𝘯 𝘴𝘵𝘢𝘵𝘦, 𝘌𝘯𝘦𝘳𝘨𝘺 𝘣𝘶𝘥𝘨𝘦𝘵, 𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 and 𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦
 #Each thematic section is made up of collections which, in order, are:
@@ -17,22 +17,16 @@
 #𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 -> LAKE SURFACE WATER TEMPERATURE, LAKE WATER QUALITY,WATER BODIES and WATER LEVEL
 #𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦 -> CRYOSPHERE
 #It is possible to have a preview of the collection in which I am interested by clicking on the Collections subsection;
-# The 𝓲 at the top right instead is a link to the European Union's Earth Observation Program website where the collection I selected from the main page is in-depth;
-# By clicking on the collection from which to obtain the satellite image, a map of the globe will appear on the screen with a toolbar on the right to extrapolate the image of my interest
+#The 𝓲 at the top right instead is a link to the European Union's Earth Observation Program website where the collection I selected from the main page is in-depth
+#By clicking on the collection from which to obtain the satellite image, a map of the globe will appear on the screen with a toolbar on the right to extrapolate the image of my interest
 #The reference section is Catalog research with three subsections to facilitate the search for the user: Help, Collection and Basic
 #In the Help subsection there is the following warning: The "Catalog search" tab allows you to define search criteria and start a search within the catalog
 #In the Collection subsection there is the set of downloadable products in the reference collection
 # While in the Basic subsection the user has the possibility to choose the time period in which to carry out a hypothetical future analysis: Start date and End Date. However, with the Slot I can be in a position to investigate a range from year to year useful for multitemporal analyzes
-#The ROI instead is a tool that allows you to enter the coordinates of the area of ​​my interest or through which I get them after selecting the previous area as Draw region of interest
+#The ROI instead is a tool that allows you to enter the coordinates of the area of my interest or through which I get them after selecting the previous area as Draw region of interest
 #Search in progress wait
-# To download the satellite image of my interest - visible in preview by clicking with the left button on the thumbnail or even quicklook for a correspondence with the global map made available by Copernicu - the blue download arrow is responsible for uploading it to the indicated folder during his rescue
+#To download the satellite image of my interest - visible in preview by clicking with the left button on the thumbnail or even quicklook for a correspondence with the global map made available by Copernicus - the blue download arrow is responsible for uploading it to the indicated folder during his rescue
 
-#Visualizzaione di dati dal programma Copernicus Europe's eyes on Earth
-#Iscrizione al programma Copernicus Europe's eyes on Earth tramite sito web
-#Come scaricare un'immagine da un programma specifico?
-#Selezione del programma di mio interesse e ricerca per filtri
-#Lo scaricamento dell'immagine è accessibile se la freccia nella seconda colonna da sinistra è di colore blu!
-#Se il cursore dell'immagine è al di sopra del nome del file selezionato da scaricare è visualizzabile un'anteprima; quest'ultima potrebbe non essere disponibile e visualizzabile in una scheda prima dello scaricamento nella cartella lab per Telerilevamento!
 #Il formato nc richiede l'installazione di uno specifico pacchetto affinché si visualizzi l'immagine per analizzare i dati: install.packages("ncdf4")
 #provo con l'URL 'https://cran.mirror.garr.it/CRAN/bin/windows/contrib/4.0/ncdf4_1.17.zip'
 Content type 'application/zip' length 3267795 bytes (3.1 MB)
@@ -54,3 +48,7 @@ The downloaded binary packages are in
 #Perchè cambia lo sfondo nel plot(snowcover2021, col=sc21gbwcp) essendo verde come l'assenza di copertura nevosa?
 #Valori NA da riclassificare per non c'è associazione con valori reali copNDVI <- raster::reclassify(copNDVI, cbind(252,255, NA), right=TRUE) selenzionandoli tramite metadata dal sito web per il programma Copernicu
 #Come diminuire la risoluzione con un ricampionamento?
+##To aggregate pixels
+#Resampling
+copernicusagg <- aggregate(copernicus, fact=100) #assign name to new aggregate
+plot(copernicusagg, col=cl) #plot new aggregate using this palette
