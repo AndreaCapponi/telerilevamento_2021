a35a0fa67b34027f45c6c11daf0553d76b772066
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-08-04 19:00:13 +0200

    Update R_code_Copernicus.r

--- R_code_Copernicus.r
+++ R_code_Copernicus.r
@@ -1,54 +1,160 @@
-#What is Copernicus? 
+#What is Copernicus? 🛰️
+
 #Copernicus is the European Union’s Earth Observation Programme, which monitors our planet and its environment for the ultimate benefit of the citizens of Europe. It delivers data, information and services based on satellite Earth Observation data and insitu (non-space) data. The Programmeis funded, coordinated and managedby the European Commission in cooperation with partners such as ESA and EUMETSAT
+
 #What is the official Copernicus website?
+
 #The official Copernicus website is https://www.copernicus.eu/en
+
 #How to register a personal account on this website?
+
 #To register a personal account on the previous website anyone have to click the 𝗥𝗘𝗚𝗜𝗦𝗧𝗘𝗥 button and enter the following informations in the dedicated sections: 𝘜𝘴𝘦𝘳𝘯𝘢𝘮𝘦*, 𝘗𝘢𝘴𝘴𝘸𝘰𝘳𝘥*, 𝘊𝘰𝘯𝘧𝘪𝘳𝘮 𝘱𝘢𝘴𝘴𝘸𝘰𝘳𝘥*, 𝘍𝘪𝘳𝘴𝘵 𝘯𝘢𝘮𝘦*, 𝘓𝘢𝘴𝘵 𝘯𝘢𝘮𝘦*, 𝘌-𝘮𝘢𝘪𝘭 𝘢𝘥𝘥𝘳𝘦𝘴𝘴*, 𝘊𝘰𝘮𝘱𝘢𝘯𝘺*, 𝘋𝘰𝘮𝘢𝘪𝘯 𝘰𝘧 𝘢𝘤𝘵𝘪𝘷𝘪𝘵𝘺*, 𝘛𝘺𝘱𝘦 𝘰𝘧 𝘰𝘳𝘨𝘢𝘯𝘪𝘻𝘢𝘵𝘪𝘰𝘯* and 𝘊𝘰𝘶𝘯𝘵𝘳𝘺*
+
 #After completing the 𝘎𝘦𝘯𝘦𝘳𝘢𝘭 𝘪𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯 section, the future user will prove that he/she is not a robot throught reCAPTCHA system in the 𝘝𝘢𝘭𝘪𝘥𝘢𝘵𝘪𝘰𝘯 𝘤𝘰𝘥𝘦 section below: clicking on the box a green check (G✓) should appear! 
+
 #To conclude the 𝘙𝘦𝘨𝘪𝘴𝘵𝘳𝘢𝘵𝘪𝘰𝘯 𝘧𝘰𝘳𝘮, in the 𝘛𝘦𝘳𝘮𝘴 𝘰𝘧 𝘶𝘴𝘦 section in the end terms of use and privacy policy must be accepted by ticking with the left mouse button another box but this time with a blue check (B✓)     
+
 #Clicking the 𝗥𝗘𝗚𝗜𝗦𝗧𝗘𝗥 button should appear the following message: By registering in this website you are deemed to have accepted the T&C for Sentinel data use
+
 #After this message appears, the user will receive an account activation link in the mailbox indicated in the appropriate section with the information previously entered
+
 #Attention: the activation of an account in the Copernicus programme is a prerequisite for downloading satellite images on it
+
 #How to download a satellite image from the official website of the Copernicus program?
+
 #The satellite image that the user would like to download is to be identified within the thematic sections contained in the main page: 𝘝𝘦𝘨𝘦𝘵𝘢𝘵𝘪𝘰𝘯 𝘴𝘵𝘢𝘵𝘦, 𝘌𝘯𝘦𝘳𝘨𝘺 𝘣𝘶𝘥𝘨𝘦𝘵, 𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 and 𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦
+
 #Each thematic section is made up of collections which, in order, are:
+
 #𝘝𝘦𝘨𝘦𝘵𝘢𝘵𝘪𝘰𝘯 𝘴𝘵𝘢𝘵𝘦 -> VEGETATION PROPERTIES VEGETATION INDICATORS DRY MATTER PRODUCTIVITY, FIRE DISTURBANCE SOIL WATER INDEX and SURFACE SOIL MOISTURE
+
 #𝘌𝘯𝘦𝘳𝘨𝘺 𝘣𝘶𝘥𝘨𝘦𝘵 -> ROOT - TOP OF CANOPY REFLECTANCE, ALBEDO and LAND SURFACE TEMPERATURE
+
 #𝘞𝘢𝘵𝘦𝘳 𝘊𝘺𝘤𝘭𝘦 -> LAKE SURFACE WATER TEMPERATURE, LAKE WATER QUALITY,WATER BODIES and WATER LEVEL
+
 #𝘊𝘳𝘺𝘰𝘴𝘱𝘩𝘦𝘳𝘦 -> CRYOSPHERE
+
 #It is possible to have a preview of the collection in which I am interested by clicking on the Collections subsection;
+
 #The 𝓲 at the top right instead is a link to the European Union's Earth Observation Program website where the collection I selected from the main page is in-depth
+
 #By clicking on the collection from which to obtain the satellite image, a map of the globe will appear on the screen with a toolbar on the right to extrapolate the image of my interest
+
 #The reference section is Catalog research with three subsections to facilitate the search for the user: Help, Collection and Basic
+
 #In the Help subsection there is the following warning: The "Catalog search" tab allows you to define search criteria and start a search within the catalog
+
 #In the Collection subsection there is the set of downloadable products in the reference collection
-# While in the Basic subsection the user has the possibility to choose the time period in which to carry out a hypothetical future analysis: Start date and End Date. However, with the Slot I can be in a position to investigate a range from year to year useful for multitemporal analyzes
+
+#While in the Basic subsection the user has the possibility to choose the time period in which to carry out a hypothetical future analysis: Start date and End Date. However, with the Slot I can be in a position to investigate a range from year to year useful for multitemporal analyzes
+
 #The ROI instead is a tool that allows you to enter the coordinates of the area of my interest or through which I get them after selecting the previous area as Draw region of interest
+
 #Search in progress wait
+
 #To download the satellite image of my interest - visible in preview by clicking with the left button on the thumbnail or even quicklook for a correspondence with the global map made available by Copernicus - the blue download arrow is responsible for uploading it to the indicated folder during his rescue
 
-#Il formato nc richiede l'installazione di uno specifico pacchetto affinché si visualizzi l'immagine per analizzare i dati: install.packages("ncdf4")
-#provo con l'URL 'https://cran.mirror.garr.it/CRAN/bin/windows/contrib/4.0/ncdf4_1.17.zip'
-Content type 'application/zip' length 3267795 bytes (3.1 MB)
-downloaded 3.1 MB
+#install.packages() is a function which download and install packages from CRAN-like repositories or from local files
 
-package ‘ncdf4’ successfully unpacked and MD5 sums checked
+#The first package to install is raster through which is possible reading, writing, manipulating, analyzing and modeling of spatial data. The package implements basic and high-level functions for raster data and for vector data operations such as intersections:
 
-The downloaded binary packages are in
-        C:\Users\PC\AppData\Local\Temp\RtmpSKE0X0\downloaded_packages
+install.packages("raster")
 
-#library(raster)
-#library(ncdf4)
-#setwd("C:/lab/")
-#snowcover2021 <- raster("c_gls_SCE500_202104010000_CEURO_MODIS_V1.0.1.nc")
-#snowcover2021
-#plot(snowcover2021)
-#sc21gbwcp <- colorRampPalette(c('green','brown','white'))(1000)
-#plot(snowcover2021, col=sc21gbwcp)
-#Perchè cambia lo sfondo nel plot(snowcover2021, col=sc21gbwcp) essendo verde come l'assenza di copertura nevosa?
-#Valori NA da riclassificare per non c'è associazione con valori reali copNDVI <- raster::reclassify(copNDVI, cbind(252,255, NA), right=TRUE) selenzionandoli tramite metadata dal sito web per il programma Copernicu
-#Come diminuire la risoluzione con un ricampionamento?
-##To aggregate pixels
-#Resampling
-copernicusagg <- aggregate(copernicus, fact=100) #assign name to new aggregate
-plot(copernicusagg, col=cl) #plot new aggregate using this palette
+#The second package to install is ncfd4, which provides a high-level R interface to data files written using Unidata's netCDF library (version 4 or earlier), which are binary data files that are portable across platforms and include metadata information in addition to the data sets. Using this package, netCDF files (either version 4 or ``classic'' version 3) can be opened and data sets read in easily:
+
+install.packages("ncfd4")
+
+#library() is a function which load and attach add-on packages, in this case the previous raster and ncfd4 packages:
+
+library(raster)
+
+library(ncfd4)
+
+#In my computer R must use the folder named simply lab and I exploit a function (setwd) for the association between R and lab folder:
+
+setwd("C:/lab/")
+
+#In a different way from the R_remote_code_sensing_first.r, although I still exploit a function to import data from greenland subfolder - external - to R - internal¹ - this will be raster and not brick because our data does not represent a multi-layer raster object but raster layer objects!
+
+snowcover2021* <- raster("c_gls_SCE500_202104010000_CEURO_MODIS_V1.0.1.nc")
+
+#Important: the brick function creates a RasterBrick object that is a multi-layer raster object typically from a multi-layer (band) file. Instead the raster function creates a RasterLayer object from scratch, a file, an Extent object, a matrix, an 'image' object, or from a Raster *, Spatial *, im (spatstat) asc, kasc (adehabitat *), grf (geoR) or kde object!
+
+#In R, to visualize information of snowcover2021, name of it followed by Enter ↵ as physical command by keyboard:
+
+snowcover2021
+
+#The relatively snowcover2021 information is contained within the table:
+
+class      : RasterLayer 
+dimensions : 7400, 12200, 90280000  (nrow, ncol, ncell)
+resolution : 0.005, 0.005  (x, y)
+extent     : -11, 50, 35, 72  (xmin, xmax, ymin, ymax)
+crs        : +proj=longlat +datum=WGS84 +no_defs 
+source     : c_gls_SCE500_202104010000_CEURO_MODIS_V1.0.1.nc 
+names      : Snow.Cover.Extent 
+z-value    : 2021-04-01 
+zvar       : sce500 
+
+#plot is a common function for plotting of R object and in this case I exploit the previous to visualize spectral bands:
+
+plot(snowcover2021)
+
+#I exploit a function (colorRampPalette) to create a new palette of colors each one of them is indexed: numbered pixel as virtual box matches numbered color as bit depth:
+
+sc21gbwcp <- colorRampPalette(c('green','brown','white'))(1000)
+
+#plot(plot(snowcover2021) must be reformulated by incorporating new palette of colors sc21gbwcp: plot(snowcover2021, col=sc21gbwcp) will display graphically reflectance's values in a green, brown and black colour scale:
+
+plot(snowcover2021, col=sc21gbwcp)
+
+#Why does the background in the plot change (snowcover2021, col = sc21gbwcp) is green as if there is no snow cover?
+
+#The N̲ot A̲ssigned values (NA) in the background of the plot (snowcover2021, col = sc21gbwcp) are to be reclassified because there is no association with the values in reality!
+
+#reclassify() is a function which reclassify values of a Raster* object. The function (re)classifies groups of values to other values. For example, all values between 1 and 10 become 1, and all values between 11 and 15 become 2:
+
+snowcover2021reclassified <- raster::reclassify(snowcover2021, cbind(252,255, NA), right=TRUE)
+
+#plot is a common function for plotting of R object and in this case I exploit the previous to visualize snowcover2021reclassified without a colored green background because of the reclassification in NA values:
+
+plot(snowcover2021reclassified)
+
+#Using the aggregate () function it is possible to reduce the resolution in snowcover2021reclassified by resampling instead of preserving it as shown in 5. R code multivariate analysis?
+
+#The function aggregate() splits the data into subsets, computes summary statistics for each, and returns the result in a convenient form:
+
+sc21ra <- aggregate(snowcover2021reclassified, fact=100) 
+
+#plot(plot(sc21ra) must be reformulated by incorporating new palette of colors sc21gbwcp: plot(sc21ra, col=sc21gbwcp) will display graphically reflectance's values in a green, brown and black colour scale:
+
+plot(sc21ra, col=sc21gbwcp)
+
+#The plot () function shows the difference in resolution between snowcover2021reclassified and sc21ra: scr21ra resolution < snowcover2021reclassified resolution. The resampling of its pixels in an image depending on the attributed reflectance values ​​could compromise its readability and / or interpretation!
+
+#Sequence of informatic commands for R_code_Copernicus.r:
+
+install.packages("raster")
+
+install.packages("ncdf4")
+
+library(raster)
+
+library(ncdf4)
+
+snowcover2021 <- raster("c_gls_SCE500_202104010000_CEURO_MODIS_V1.0.1.nc")
+
+snowcover2021
+
+plot(snowcover2021)
+
+sc21gbwcp <- colorRampPalette(c('green','brown','white'))(1000)
+
+plot(snowcover2021, col=sc21gbwcp)
+
+snowcover2021reclassified <- raster::reclassify(snowcover2021, cbind(252,255, NA), right=TRUE)
+
+plot(snowcover2021reclassified)
+
+sc21ra <- aggregate(snowcover2021reclassified, fact=100) 
+
