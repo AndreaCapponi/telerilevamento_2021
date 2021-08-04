b30019b2f2b54aa209de328907eb84bc20a64190
AndreaCapponi <80782959+AndreaCapponi@users.noreply.github.com>
2021-08-04 07:03:39 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,17 +1,35 @@
 #Al di sotto di questo commento studiare e organizzare la lezione del 14/04/2021
-#install.packages("RStoolbox")
-#library(RStoolbox)
-#install.packages('knitr', dependencies = TRUE)
-#library(knitr)
-#stitch function is required!
-#stitch: Automatically create a report based on an R script and a template
-#This is a convenience function for small-scale automatic reporting based on an R script and a template. The default template is an Rnw file (LaTeX); stitch_rhtml() and stitch_rmd() are wrappers on top of stitch() using the R HTML and R Markdown templates respectively.
-#setwd("C:/lab/")
-#Knitr è interno a R sfrutta un codice esterno per creare un report (inverted commas) importandolo nel programma* per successivamente salvarlo come PDF nella cartella dove era collocato il codice precedente!
-#library(knitr)
-#Il codice viene salvato in un documento di testo all'interno della cartella lab riportando il nome del repository del quale noi vogliamo creare il report perchè *
-#Com'è la sintassi della funzione stitch?
-#stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#install.packages() is a function which download and install packages from CRAN-like repositories or from local files
+
+#The first package to install is RStoolbox, a toolbox for remote sensing image processing and analysis such as calculating spectral indices, principal component transformation, unsupervised and supervised classification or fractional cover analyses:
+
+install.packages("RStoolbox")
+
+#The second package to install is knitr, a general-purpose tool for dynamic report generation in R using Literate Programming techniques:
+
+install.packages('knitr', dependencies = TRUE)
+
+#library() is a function which load and attach add-on packages, in this case the previous RStoolbox and knitr packages:
+
+library(RStoolbox)
+
+library(knitr)
+
+#I might as well consider require() function, giving a warning message and returning a logical value as FALSE if the requested package is not found and TRUE if the package is loaded. The library() function instead by default returns an error if the requested package does not exist!
+
+#In my computer R must use the folder named simply lab and I exploit a function (setwd) for the association between R and lab folder:
+
+setwd("C:/lab/")
+
+# stitch() is a convenience function that creates automatically a small-scale report based on a R script and a template:
+
+stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#The future script will be R_code_Greenland.r.txt and the default template is an Rnw file (LaTeX); stitch_rhtml() and stitch_rmd() are wrappers on top of stitch() using the R HTML and R Markdown templates respectively!
+
+#knitr is conceptually internal to R but in creating a report it is based on an external code (inverted commas). After importing it into R, this function saves the complete code as a text document inside the folder - indicated instead with the previous setwd () function - with a title corresponding to the name of the selected repository (R_code_Greenland)
+
 #Discordanza tra sintassi GitHub e R da risolvere andando a specificare il formato del documento di testo nel quale si ha il codice da applicare per creare il report!
 #R_code_Greenland.r.tex, perchè non è il file PDF finale?
 #Errore: LaTeX failed to compile R_code_Greenland.r.tex. See https://yihui.org/tinytex/r/#debugging for debugging tips.
